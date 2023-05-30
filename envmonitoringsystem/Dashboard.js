06454c5ff60a8270f102d3810076969bb04b7247
Samiur Rahman Alif <88434843+SamiurRahmanAlif@users.noreply.github.com>
2023-05-30 19:26:09 +0600

    ip

--- envmonitoringsystem/Dashboard.js
+++ envmonitoringsystem/Dashboard.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
-export default function Dasbboard  ({ navigation }) {
+import React from "react";
+import { StyleSheet, View, Text, ImageBackground, image } from "react-native";
+export default function Dasbboard({ navigation }) {
   return (
-    <View style={styles.container}>
-
-      {/* 1st div */}
-      <View style={styles.row}>
-        <View style={styles.box}></View>
-        <View style={styles.box}></View>
-      </View>
-
-      {/* 2nd div */}
-      <View style={styles.row}>
-        <View style={styles.box}>
+    <View>
+      <View style={styles.container}>
+        <Image
+          style={styles.backgroundImage}
+          source={require("/assets/Picture1.png")}
+        />
+        {/* 1st div */}
+        <View style={styles.row}>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
         </View>
-        <View style={styles.box}></View>
-      </View>
 
-      {/* 3rd div */}
-      <View style={styles.row}>
-        <View style={styles.box}></View>
-        <View style={styles.box}></View>
+        {/* 2nd div */}
+        <View style={styles.row}>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+        </View>
+
+        {/* 3rd div */}
+        <View style={styles.row}>
+          <View style={styles.box}></View>
+          <View style={styles.box}></View>
+        </View>
       </View>
     </View>
   );
@@ -30,20 +33,29 @@ export default function Dasbboard  ({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100vh",
+    maxHeight: "maxcontent",
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover", // or 'stretch' if you want to stretch the image
+    position: "absolute",
+    width: "100%",
+    height: "100%",
   },
   row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '90%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
     marginBottom: 20,
   },
   box: {
-    width: '45%',
+    width: "45%",
     height: 100,
-    backgroundColor: 'red',
+    backgroundColor: "red",
     borderRadius: 20,
   },
 });
